ce6d5d51b9b0edbfa10bc43efecfddf8ec95e13a
Anu <anu.viswan@gmail.com>
2017-09-05 19:25:16 +0530

    Updated Button CSS

--- src/web/REngine.Admin/src/app/layout/user/user-routing.module.ts
+++ src/web/REngine.Admin/src/app/layout/user/user-routing.module.ts
@@ -3,17 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { UserListComponent } from './user-list/user-list.component';
 import { UserComponent } from './user.component';
-
+import { ViewUserComponent } from './view-user/view-user.component';
 const routes: Routes = [
     { path: '', component: UserComponent,
     children: [ 
 	    {
-		path: 'userlist',
-		component: UserListComponent
+			path: '',
+			component: UserListComponent
+	    },
+	    {
+			path: 'userlist',
+			component: UserListComponent
+	    },
+	    {
+	    	path: 'viewuser',
+	    	component: ViewUserComponent
 	    }
          ] 
-     },
-
+	    }
 ];
 
 @NgModule({
--- src/web/REngine.Admin/src/app/layout/user/user.module.ts
+++ src/web/REngine.Admin/src/app/layout/user/user.module.ts
@@ -7,6 +7,8 @@ import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user-routing.module';
 import { PageHeaderModule } from './../../shared';
 import { UserListComponent } from './user-list/user-list.component';
+import { ViewUserComponent } from './view-user/view-user.component';
+
 
 @NgModule({
     imports: [
@@ -15,6 +17,6 @@ import { UserListComponent } from './user-list/user-list.component';
         DataTablesModule,
         PageHeaderModule
     ],
-    declarations: [UserComponent, UserListComponent]
+    declarations: [UserComponent, UserListComponent, ViewUserComponent]
 })
 export class UserModule { }
